22d4acb894fc11e10ee4faae68fe1aa24c7ca919
Yufei Guo <812773673@qq.com>
2017-04-01 17:23:30 +0800

    python核心编程第十三章第二大题

--- 0001.py
+++ 0001.py
@@ -1,12 +1,27 @@
 # coding=utf-8
+import random
+import string
 
-import Image,ImageDraw,ImageFont
+L = []
 
-pic=Image.open('0001.jpg')#打开图片作为图片对象
-ttf = '/usr/share/fonts/truetype/fonts-japanese-gothic.ttf' #字体路径
-font=ImageFont.truetype(ttf,size=50) #打开字体作为字体对象
-fill='#ff0000' #字体填充颜色
-draw=ImageDraw.Draw(pic) #将图片对象作为画布对象
-draw.text((160,0),chr(52),fill=fill,font=font) #在画布对象上添加文字
 
-pic.save('test.png','PNG') #将图片对象保存为png格式
+def Activetion_code():#通过随机函数得到一个大小写和数字混合的激活码
+    code=[]
+    for i in range(9):
+        if i == 0:
+            x=random.choice(string.uppercase+string.lowercase)#激活码首位为字母
+            code.append(x)
+        else:
+            x=random.choice(string.uppercase+string.lowercase+string.digits*2)
+            code.append(x)
+    code=''.join(code)
+    return code
+
+def code_200():#生成包含200个激活码的列表
+    code_list= L
+    for i in range(200):
+        x=Activetion_code()
+        code_list.append(x)
+    return code_list
+
+print code_200()
